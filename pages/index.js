03c1d5e49dd1c27b5d606444d5307c5e8887ef60
unknown <callmetyer@gmail.com>
2023-01-07 20:22:57 +0700

    baitap

--- pages/index.js
+++ pages/index.js
@@ -58,8 +58,8 @@ router.push({
       ))}
 
       <img
-        src="https://scontent.fsgn5-13.fna.fbcdn.net/v/t39.30808-6/296099477_101442889337580_4175294896963945968_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=ghIAAjqj_d8AX_LpKt3&tn=LXo4bNndAb8GCCKs&_nc_ht=scontent.fsgn5-13.fna&oh=00_AfAEktBHSMmK2WN-6wD-AF7sbaRx4qsBzKSOf90txWIh5w&oe=63AD3375"
-        alt="13"
+        src="https://vatvostudio.vn/wp-content/uploads/2022/09/rogphone6d.jpeg" 
+        alt="Ảnh"
         width={500}
 
       />
