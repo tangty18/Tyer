bd0d72a60f56d7cbbd30fe4b4bf7b4d6d67756d2
unknown <ngoc_tu@hydrax.io>
2023-01-09 21:46:14 +0700

    add more layout

--- components/header/header1.js
+++ components/header/header1.js
@@ -1,5 +1,26 @@
 import styles from "./header1.module.css";
+import { useRouter } from "next/router";
 
 export function Header1() {
-  return <div className={styles.header1}>HEADER 1</div>;
+  const router = useRouter();
+
+  function xemBut(){
+    router.push({
+        pathname: "/but",
+      });
+  }
+
+  function xemQuanAo(){
+    router.push({
+        pathname: "/quanao",
+      });
+  }
+
+
+  return (
+    <div className={styles.header1}>
+      <div className={styles.item} onClick={xemBut}>Xem bút</div>
+      <div className={styles.item} onClick={xemQuanAo}>Xem quần áo</div>
+    </div>
+  );
 }
