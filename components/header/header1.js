23007d72c9c070e92e3cd8c1c438a8c8e8feffcf
unknown <callmetyer@gmail.com>
2023-01-09 21:45:44 +0700

    demo

--- components/header/header1.js
+++ components/header/header1.js
@@ -1,5 +1,5 @@
 import styles from "./header1.module.css";
 
 export function Header1() {
-  return <div className={styles.header1}>HEADER 1</div>;
+  return <div className={styles.header1}>ABCD</div>;
 }
