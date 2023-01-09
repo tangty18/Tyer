52f01a527232b68b21282b124ab3fd190916e36b
unknown <ngoc_tu@hydrax.io>
2023-01-09 21:27:13 +0700

    deme layout 1

---
+++ components/footer/footer1.js
@@ -0,0 +1,5 @@
+import styles from "./footer1.module.css";
+
+export function Footer1() {
+    return <div className={styles.footer1}>FOOTER 1</div>;
+}
---
+++ components/header/header1.js
@@ -0,0 +1,3 @@
+export function Header1() {
+  return <div>HEADER 1</div>;
+}
