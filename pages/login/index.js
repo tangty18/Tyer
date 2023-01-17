dd03017b34bc9d822fc9424cccb18c1a750e3ad8
Is Me Tyer <callmetyer@gmail.com>
2023-01-17 07:06:47 +0700

    thêm form

--- pages/login/index.js
+++ pages/login/index.js
@@ -2,17 +2,22 @@ import { useState } from "react";
 import { Layout1 } from "../../components/layout/layout1";
 
 export default function DangNhap() {
-return(
+  return (
     <div>
-        123
+      <form>
+        <label for="fname">First name:</label><br></br>
+        <input type="text" id="fname" name="fname" value="John" /><br></br>
+        <label for="lname">Last name:</label><br />
+        <input type="text" id="lname" name="lname" value="Doe" /><br></br>
+        <input type="submit" value="Submit"></input>
+      </form>
     </div>
-)
+  )
 }
 DangNhap.getLayout = function getLayout(page) {
-    return (
-      <Layout1>
-        <DangNhap />
-      </Layout1>
-    );
-  };
-  
+  return (
+    <Layout1>
+      <DangNhap />
+    </Layout1>
+  );
+};
