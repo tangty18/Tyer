0bb24ac3ef28e292d528e4bd02018535881cd040
Is Me Tyer <callmetyer@gmail.com>
2023-01-17 07:09:22 +0700

    thay đổi đang nhập

--- pages/login/index.js
+++ pages/login/index.js
@@ -5,9 +5,9 @@ export default function DangNhap() {
   return (
     <div>
       <form>
-        <label for="fname">First name:</label><br></br>
+        <label for="fname">Your Name:</label><br></br>
         <input type="text" id="fname" name="fname" value="John" /><br></br>
-        <label for="lname">Last name:</label><br />
+        <label for="lname">PassWord:</label><br />
         <input type="text" id="lname" name="lname" value="Doe" /><br></br>
         <input type="submit" value="Submit"></input>
       </form>
