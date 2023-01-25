4b98d542ae8898b4b571fa8f169280387b39af3b
DESKTOP-LR9LHM6\Win 10 <nntu079@gmail.com>
2023-01-25 20:39:06 +0700

    them yeu thich

--- pages/api/but/lay-toan-bo.js
+++ pages/api/but/lay-toan-bo.js
@@ -1,6 +1,7 @@
 export default function handler(req, res) {
   res.status(200).json([
     {
+      id: 1,
       name: "Bút Chì",
       soLuong: 23000,
       khoiLuong: 13,
@@ -8,6 +9,7 @@ export default function handler(req, res) {
         "http://img.websosanh.vn/v2/users/root_product/images/but-chi-go-2b-thien-long-gp02/2jgr174h4dbhl.jpg",
     },
     {
+      id: 2,
       name: "Bút Lông",
       soLuong: 15000,
       khoiLuong: 13,
@@ -15,6 +17,7 @@ export default function handler(req, res) {
         "https://product.hstatic.net/1000230347/product/but_long_dau_thien_long_pm-09__1_.jpg",
     },
     {
+      id: 3,
       name: "Bút Màu",
       soLuong: 50000,
       khoiLuong: 13,
@@ -22,6 +25,7 @@ export default function handler(req, res) {
         "https://phucma.com.vn/wp-content/uploads/2019/06/but-mau-noi-Artline-Pastel-ink-min.jpg",
     },
     {
+      id: 4,
       name: "Bút Bi",
       soLuong: 50000,
       khoiLuong: 13,
@@ -36,12 +40,14 @@ export default function handler(req, res) {
         "https://phucma.com.vn/wp-content/uploads/2019/06/but-mau-noi-Artline-Pastel-ink-min.jpg",
     },
     {
+      id: 5,
       name: "Bút Chì",
       soLuong: 23000,
       image:
         "http://img.websosanh.vn/v2/users/root_product/images/but-chi-go-2b-thien-long-gp02/2jgr174h4dbhl.jpg",
     },
     {
+      id: 6,
       name: "Bút Lông",
       soLuong: 15000,
       khoiLuong: 13,
@@ -49,6 +55,7 @@ export default function handler(req, res) {
         "https://product.hstatic.net/1000230347/product/but_long_dau_thien_long_pm-09__1_.jpg",
     },
     {
+      id: 7,
       name: "Bút Màu",
       soLuong: 50000,
       khoiLuong: 13,
@@ -63,6 +70,7 @@ export default function handler(req, res) {
         "https://adcbook.net.vn/web/image/product.template/74838/image_1024?unique=2c1660d",
     },
     {
+      id: 8,
       name: "Cọ Vẽ",
       soLuong: 6000,
       khoiLuong: 13,
