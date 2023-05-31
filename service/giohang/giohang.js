4b5b074eef68ae7e678ec06938582df8ac287b98
Is Me Tyer <callmetyer@gmail.com>
2023-05-31 21:07:06 +0700

    --saved

---
+++ service/giohang/giohang.js
@@ -0,0 +1,24 @@
+import conn from "../../tools/pg"
+import {v4 as uuidv4} from 'uuid';
+
+
+export async function themSanPhamVaoGioHang(username,id_product,quantity){
+    const id = uuidv4()
+    let res = await conn.query(
+     `INSERT INTO giohang(
+       id, id_sanpham, username, soluong)
+       VALUES ($1, $2, $3, $4);`,
+       [id,id_product,username,quantity]
+    )
+    return res.rows;
+}
+
+export async function layHet(username) {
+    let res = await conn.query(
+      `SELECT *
+       FROM giohang
+       WHERE username = $1`,
+      [username]
+    );
+    return res.rows;
+  }
